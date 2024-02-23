e9d3c8f6d8dc90bcf46e2ad496198f605236151c
Nobie120 <n12507400@gmail.com>
2024-02-23 05:05:44 -0800

    Improved engine a wee bit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,9 +61,31 @@ class FileStorage:
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = obj.__class__.__name__ + '.' + id
             if key in self.__objects:
                 del self.__objects[key]
+
+
+    def get(self, cls, id):
+        """ retrieve one object based on Id """
+        key = cls.__name__ + '.' + id
+        if key in self.__objects.keys():
+            return self.__objects[key]
+        else:
+            return None
+
+    def count(self, cls=None):
+        """count number of objects in storage"""
+        count = 0
+        if cls is not None:
+            for key, value in self.__objects.items():
+                if value.__class__ == cls:
+                    count += 1
+        else:
+            for key, value in self.__objects.items():
+                count += 1
+        return count
+
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
